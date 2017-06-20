a924fb1487c798bb76f5b446429d9d28e919c196
fengpeng5 <alfred.pan@foxmail.com>
2017-06-20 15:56:34 +0800

    fix warning and fix dist file size

---
+++ app/reducers/index.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux';
+
+const editorReducers = combineReducers({
+  startEdit
+});
+
+
+function startEdit (previousState, action) {
+  return {};
+}
+
+
+export default editorReducers;
