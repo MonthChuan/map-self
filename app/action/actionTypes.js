8470dc181bca48d69f8a257741c82f4c9f9d042a
luoqiuyue <luoqiuyue@wanda.cn>
2017-07-27 10:44:15 +0800

    添加保存user信息

--- app/action/actionTypes.js
+++ app/action/actionTypes.js
@@ -11,3 +11,4 @@ export const SET_CONFIRMSHOW = 'setConfirmShow';
 export const INCREASE_MAXNUM = 'increaseMaxNum';
 export const GET_STORECATGORY = 'getStoreCatgory';
 export const RESET_STORE = 'resetStore';
+export const SET_USERINFO = 'setUserInfo';
